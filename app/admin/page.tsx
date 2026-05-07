58699bbada4b16f0c46af1d3bad969144c64cea2
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2026-05-07 14:40:00 +0200

    ajout bouton "se deconnecter"

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+
 export default function Admin() {
   return (
     <>
       <h1>Page Admin Dashboard</h1>
       <p>cette page est sécurisée.</p>
+
+      <hr />
+
+      <button onClick={() => signOut({callbackUrl: "/login"})}>
+        Se déconnecter
+      </button>
     </>
   );
 }
