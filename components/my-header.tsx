ae2471371c20e2e0d48efb213d6c5822739bfb90
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2026-05-07 09:53:09 +0200

    1st commit

---
+++ components/my-header.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function MyHeader() {
+  return (
+    <header className="border border-amber-600">
+        <nav className='w-full max-w-7xl py-2 px-4 mx-auto flex gap-5'>
+          <Link href="/">Accueil</Link>
+          <Link href="/informations">Informations</Link>
+          <Link href="/contact">Contact</Link>
+          <Link href="/admin">Admin</Link>
+        </nav>
+    </header>
+  );
+}
