8e7ed961fdb59e046d4ffd4c0cca847ba49410de
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2026-05-07 14:39:48 +0200

    securisation des pages "admin/"

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,24 @@
-export default function RootLayout({
+
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect("/login");
+  }
+  
   return (
-    <section className="border border-fuchsia-500">
-      {children}
-    </section>
+    <>    
+      <section className="border border-fuchsia-500">
+        {children}
+      </section>
+    </>
   );
 }
