47e56a481d80cfb5fa7da2746d706033f0cd8c1c
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2026-06-18 15:13:21 +0200

    domain: apiClient gestion response null

--- domain/apiClient.ts
+++ domain/apiClient.ts
@@ -1,8 +1,14 @@
+const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL!
+
 export async function apiClient<T>(route: string, options?: RequestInit): Promise<T> {
-  const response = await fetch(`http://localhost:3000/api/${route}`, options);
+  const response = await fetch(NEXT_PUBLIC_API_URL + `/${route}`, options);
 
   if (!response.ok) {
     throw new Error(`API error: ${response.status}`);
+  }
+
+  if (response.status === 204) {
+    return null as T;
   }
 
   return response.json();
